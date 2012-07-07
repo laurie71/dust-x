827a1e56f0aea3af66e10b88306e5dcdc996cff4
Samuel Merry <sam@asdf.local>
2012-07-06 18:07:12 -0700

    adding line endings and updating to dustjs-linkedin

--- dust-x.js
+++ dust-x.js
@@ -3,21 +3,21 @@
 // Based in part on express-dust by Dav Glass
 // https://github.com/davglass/express-dust
 
-var dust = require('dust')
+var dust = require('dustjs-linkedin')
   , http = require('http')
   , View = require('express/lib/view.js')
 
 // ------------------------------------------------------------------------
 
-var DEBUG = false
+var DEBUG = false;
 
 var debug = DEBUG
     ? console.error.bind(console, 'DUSTX DEBUG')
-    : function() {}
+    : function() {};
     
 var summary = function(str) {
     return str.split('\n')[0].slice(0, 50)+'...'
-}
+};
 
 // ------------------------------------------------------------------------
     
@@ -31,20 +31,20 @@ dust.optimizers.format = function(context, node) {
 // This is used when a Dust template references another template, e.g. with:
 // {<template}
 dust.onLoad = function onLoad(name, context, callback) {
-    debug('onLoad', name)
+    debug('onLoad', name);
 
     // var v = view.lookup(name, context.stack.head)
-    var v = view_lookup(name, context.stack.head)
+    var v = view_lookup(name, context.stack.head);
     
-    debug('onLoad ->', summary(v.contents))
-    callback(null, v.contents)
-}
+    debug('onLoad ->', summary(v.contents));
+    callback(null, v.contents);
+};
 
 // Monkey-patch Dust's load function to pass context through to onLoad.
 // Needed so dust.onLoad() can pass the required settings and options
 // to Express for template loading.
 dust.load = function(name, chunk, context) {
-  debug('load', name)
+  debug('load', name);
   var tmpl = dust.cache[name];
   if (tmpl) {
     return tmpl(chunk, context);
@@ -54,7 +54,7 @@ dust.load = function(name, chunk, context) {
         //dust.onLoad(name, function(err, src) {// PATCH:
         dust.onLoad(name, context, function(err, src) {
           if (err) {
-              debug('ERROR:', 'onLoad callback', err)
+              debug('ERROR:', 'onLoad callback', err);
               return chunk.setError(err);
           }
           if (!dust.cache[name]) dust.loadSource(dust.compile(src, name));
@@ -62,7 +62,7 @@ dust.load = function(name, chunk, context) {
         });
       });
     }
-    debug('ERROR: no onLoad defined')
+    debug('ERROR: no onLoad defined');
     return chunk.setError(new Error("Template Not Found: " + name));
   }
 };
@@ -73,18 +73,18 @@ dust.load = function(name, chunk, context) {
 // Note: this is called by Express when rendering a view/partial, but is
 // *not* called by Dust for partials referenced by Dust's built-in syntax.
 this.compile = function(str, opts) {
-    var name = opts.filename
-    debug('compile', name, '\n\t', summary(str))
+    var name = opts.filename;
+    debug('compile', name, '\n\t', summary(str));
 
     // If template caching is disabled, we need to completely clear the cache.
     // Just removing `name` from the cache isn't sufficient, as Dust doesn't
     // go through this function for loading template partials (see above).
-    dust.cache = {} // XXX make dependant on opts.cache / app.env == dev / ???
+    dust.cache = {}; // XXX make dependant on opts.cache / app.env == dev / ???
     
-    dust.loadSource(dust.compile(str, name))
+    dust.loadSource(dust.compile(str, name));
     
     return function render(opts) {
-        debug('render', name)
+        debug('render', name);
         var _dust = opts._dust
           , res = _dust.res
           , next = _dust.next
@@ -93,32 +93,33 @@ this.compile = function(str, opts) {
           , callback = _dust.callback
 
         dust.render(name, opts, function onrender(err, html) {
-            debug('onrender', name, '->', summary(html))
-            if (err) return next(err)
+            debug('onrender', name, '->', summary(html));
+            if (err) return next(err);
 
             // async version of tail end of res._render logic:
             if (layout) {
-                opts.isLayout = true
-                opts.layout = false
-                opts.body = html
-                this.render(layout, options, callback, view, true) // fixme ??? needs testing
+                opts.isLayout = true;
+                opts.layout = false;
+                opts.body = html;
+                this.render(layout, options, callback, view, true); // fixme ??? needs testing
             } else if (partial) {
-                callback(null, html) // fixme is this the right thing to do here?
+                callback(null, html); // fixme is this the right thing to do here?
             } else if (callback) {
-                callback(null, html)
+                callback(null, html);
             } else {
-                res.send(html)
+                res.send(html);
             }
-        })
-    }
-}
+        });
+    };
+};
 
 // We need to monkey-patch Express's _render, too, so we can access the
 // request, response and next() function from this.compile() and prevent
 // Express from res.send()'ing the result of the template function, which
 // we need to do asyncronously.
 var _render = http.ServerResponse.prototype.render
-  , _onerror = function(e) { if (e) throw(e) }
+  , _onerror = function(e) { if (e) throw(e); };
+
 http.ServerResponse.prototype.render = function render(xview, opts, fn, parent, sub) {
     if (typeof(options) == 'function') {
         fn = opts;
@@ -129,23 +130,23 @@ http.ServerResponse.prototype.render = function render(xview, opts, fn, parent, 
     // to store opts.layout and opts.isPartial and set both to false to
     // prevent Express from handling those syncronously, then handle them
     // during render
-    opts = opts || {}
-    opts._dust = {}
-    opts._dust.res = this
-    opts._dust.req = this.req
-    opts._dust.next = this.req.next
-    opts._dust.callback = fn
+    opts = opts || {};
+    opts._dust = {};
+    opts._dust.res = this;
+    opts._dust.req = this.req;
+    opts._dust.next = this.req.next;
+    opts._dust.callback = fn;
 
-    opts._dust.layout = opts.layout
-    opts.layout = false
-    
-    opts._dust.isPartial = opts.isPartial
-    opts.isPartial = false
+    opts._dust.layout = opts.layout;
+    opts.layout = false;
+
+    opts._dust.isPartial = opts.isPartial;
+    opts.isPartial = false;
     
     // call default render, passing a no-op callbackto prevent Express from 
     // calling res.send() 
-    _render.call(this, xview, opts, _onerror, parent, sub)
-}
+    _render.call(this, xview, opts, _onerror, parent, sub);
+};
 
 // ------------------------------------------------------------------------
 
@@ -164,7 +165,8 @@ function view_lookup(name, options) {
     }
     
     // Populate view
-    var view, orig = view = new View(name, options);
+    var view;
+    orig = view = new View(name, options);
 
     // Try _ prefix ex: ./views/_user.jade
     if (!view.exists) view = new View(orig.prefixPath, options);
@@ -186,7 +188,7 @@ function view_lookup(name, options) {
       throw new Error('failed to locate view "' + orig.view + '"');
     }
     
-    return view
+    return view;
 }
 
 // ------------------------------------------------------------------------
